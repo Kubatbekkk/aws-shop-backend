38257b4408fd92644c67d7adf14997f8a9270353
Kubatbekkk <kubineccastro@gmail.com>
2023-11-26 16:53:13 +0600

    feat: integrate dDB to stack

--- ProductService/lib/product_service-stack.ts
+++ ProductService/lib/product_service-stack.ts
@@ -1,10 +1,14 @@
 import * as cdk from "aws-cdk-lib";
 import { Cors, LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
-import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import {
+  NodejsFunction,
+  NodejsFunctionProps,
+} from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import path = require("path");
 import { SwaggerUi } from "@pepperize/cdk-apigateway-swagger-ui";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -23,20 +27,46 @@ export class ProductServiceStack extends cdk.Stack {
       },
     });
 
-    const lambdaGeneralProps = {
+    const lambdaGeneralProps: Partial<NodejsFunctionProps> = {
       runtime: Runtime.NODEJS_18_X,
       handler: "handler",
+      environment: {
+        PRODUCTS_TABLE_NAME: "products",
+        STOCKS_TABLE_NAME: "stocks",
+      },
     };
+
+    const productsTable = dynamodb.Table.fromTableAttributes(
+      this,
+      "Products Table ",
+      {
+        tableName:
+          lambdaGeneralProps.environment?.PRODUCTS_TABLE_NAME || "products",
+      }
+    );
+
+    const stocksTable = dynamodb.Table.fromTableAttributes(
+      this,
+      "Stocks Table ",
+      {
+        tableName:
+          lambdaGeneralProps.environment?.STOCKS_TABLE_NAME || "stocks",
+      }
+    );
 
     const getProductsList = new NodejsFunction(this, "getPRoductsList", {
       ...lambdaGeneralProps,
       entry: path.join(__dirname + "/../src/lambdas/getProductsList.ts"),
     });
+    productsTable.grantReadWriteData(getProductsList);
+    stocksTable.grantReadWriteData(getProductsList);
 
     const getProductById = new NodejsFunction(this, "getProductById", {
       ...lambdaGeneralProps,
       entry: path.join(__dirname + "/../src/lambdas/getProductById.ts"),
     });
+
+    productsTable.grantReadWriteData(getProductById);
 
     const products = api.root.addResource("products");
     const product = products.addResource("{id}");
