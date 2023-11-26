7ef7f6460125df8ad539baa73f3bc27ae5321729
Kubatbekkk <kubineccastro@gmail.com>
2023-11-26 17:39:36 +0600

    feat: getList from dDB

--- ProductService/src/handlers/getList.ts
+++ ProductService/src/handlers/getList.ts
@@ -1,10 +1,30 @@
 import { StatusCodes } from "http-status-codes";
-import { products } from "../mocks/products";
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
 import { response } from "../utils/response";
+import { Product } from "../models/product";
+
+const PRODUCTS_TABLE_NAME = process.env.PRODUCTS_TABLE_NAME || "products";
+
+type ProductParams = {
+  TableName: string;
+};
 
 export const getList = async () => {
+  const dDBClient = new DynamoDBClient();
+
   try {
-    return response(StatusCodes.OK, products);
+    const productParams: ProductParams = {
+      TableName: PRODUCTS_TABLE_NAME,
+    };
+
+    const productsResult = await dDBClient.send(new ScanCommand(productParams));
+
+    const unmarshalledItems = productsResult.Items?.map((item) =>
+      unmarshall(item)
+    ) as Product[];
+
+    return response(StatusCodes.OK, unmarshalledItems);
   } catch (error) {
     return response(StatusCodes.INTERNAL_SERVER_ERROR, {
       code: StatusCodes.INTERNAL_SERVER_ERROR,
