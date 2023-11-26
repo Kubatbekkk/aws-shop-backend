00e1ac6f5f55e255e05d4eb40889ff3d9207f12e
Kubatbekkk <kubineccastro@gmail.com>
2023-11-26 23:01:50 +0600

    fix: response util body arg type

--- ProductService/src/utils/response.ts
+++ ProductService/src/utils/response.ts
@@ -1,10 +1,10 @@
-import type { Product } from "../models/product";
+import type { FoundProduct, Product } from "../models/product";
 import type { Exception } from "../models/exception";
 import { CORS_ENABLE_HEADERS } from "../constants/constants";
 
 export const response = (
   statusCode: number,
-  body: Product | Product[] | Exception
+  body: Product | Product[] | Exception | FoundProduct | Record<string, any>
 ) => ({
   statusCode,
   headers: CORS_ENABLE_HEADERS,
