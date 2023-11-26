dcc9b76ad6400b0f8cf72bf1bfc3e04598cc2828
Kubatbekkk <kubineccastro@gmail.com>
2023-11-26 19:45:35 +0600

    feat: getList join stock & products

--- ProductService/src/handlers/getList.ts
+++ ProductService/src/handlers/getList.ts
@@ -3,10 +3,12 @@ import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
 import { unmarshall } from "@aws-sdk/util-dynamodb";
 import { response } from "../utils/response";
 import { Product } from "../models/product";
+import { Stock } from "../models/stock";
 
 const PRODUCTS_TABLE_NAME = process.env.PRODUCTS_TABLE_NAME || "products";
+const STOCKS_TABLE_NAME = process.env.STOCKS_TABLE_NAME || "stocks";
 
-type ProductParams = {
+type TableParams = {
   TableName: string;
 };
 
@@ -14,17 +16,33 @@ export const getList = async () => {
   const dDBClient = new DynamoDBClient();
 
   try {
-    const productParams: ProductParams = {
+    const productParams: TableParams = {
       TableName: PRODUCTS_TABLE_NAME,
     };
 
-    const productsResult = await dDBClient.send(new ScanCommand(productParams));
+    const stockParams: TableParams = {
+      TableName: STOCKS_TABLE_NAME,
+    };
 
-    const unmarshalledItems = productsResult.Items?.map((item) =>
+    const productsResult = await dDBClient.send(new ScanCommand(productParams));
+    const stockResult = await dDBClient.send(new ScanCommand(stockParams));
+
+    const unmarshalledProducts = productsResult.Items?.map((item) =>
       unmarshall(item)
     ) as Product[];
 
-    return response(StatusCodes.OK, unmarshalledItems);
+    const unmarshalledStock = stockResult.Items?.map((item) =>
+      unmarshall(item)
+    ) as Stock[];
+
+    const joinedArray = unmarshalledProducts.map((product) => {
+      const stock = unmarshalledStock.find(
+        (stockItem) => stockItem.product_id === product.id
+      );
+      return { ...product, ...stock };
+    });
+
+    return response(StatusCodes.OK, joinedArray);
   } catch (error) {
     return response(StatusCodes.INTERNAL_SERVER_ERROR, {
       code: StatusCodes.INTERNAL_SERVER_ERROR,
