7eee5cef743758b6ec419042ae73f62bc88a187b
Kubatbekkk <kubineccastro@gmail.com>
2024-01-07 21:03:20 +0600

    feat: add image processing

--- ProductService/src/models/product.ts
+++ ProductService/src/models/product.ts
@@ -1,14 +1,15 @@
 export type Product = {
-  id: string;
-  title: string;
-  description: string;
-  price: number;
-};
+    id: string
+    title: string
+    description: string
+    price: number
+    image?: string
+}
 
 export type FoundProduct = {
-  id?: string;
-  title: string;
-  price: number;
-  description: string;
-  count: number;
-};
+    id?: string
+    title: string
+    price: number
+    description: string
+    count: number
+}
